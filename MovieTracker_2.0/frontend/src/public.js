dc968aa3d653ad52cc848dbf591bc8f44e60418b
po-gurungpuja <pogurungpuja@gmail.com>
2020-12-09 14:40:14 +0545

    move css file to cssFolder

--- MovieTracker_2.0/frontend/src/public.js
+++ MovieTracker_2.0/frontend/src/public.js
@@ -1,8 +1,8 @@
-import './assets/header.css';
-import './assets/searchBox.css';
-import './assets/card.css';
+import './assets/css/header.css';
+import './assets/css/searchBox.css';
+import './assets/css/card.css';
 
-import './assets/moviedetails.css';
+import './assets/css/moviedetails.css';
 import "./assets/css/toaster.css";
 import "./assets/css/spinner.css";
 
